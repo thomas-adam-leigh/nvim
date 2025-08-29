a0b39e2f6427c0b9306743f0e0dc6094e0f38d99
t.adam.leigh@gmail.com <t.adam.leigh@gmail.com>
2025-08-29 16:26:23 +0200

    find directories now excludes node modules. This is great.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,18 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('telescope').setup{
+        defaults = {
+          file_ignore_patterns = {
+            "node_modules/.*",
+            "%.git/.*",
+            "dist/.*",
+            "build/.*",
+            "%.min%.",
+            "lock"
+          }
+        }
+      }
+    end,
+  }
